74416737a7e13a0fb266b5dcab6a1a7aaa5baf2d
Sandalu Dewthilini <150440179+SDewthilini@users.noreply.github.com>
2024-10-18 15:33:10 +0530

    Update vim animal.py

--- vim animal.py
+++ vim animal.py
@@ -7,7 +7,10 @@ def default():
     print('I am an animal')
 
 def main():
-    default()
+    if sys.argv[1] == 'cat':
+        cat()
+    else:
+        default()
 
 if __name__ == '_main_':
     main()
